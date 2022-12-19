8e1fdf51efd2c34ffe9764158b00fb269ec4c86b
Fabricio Varisco <fabricio.varisco@outlook.com>
2022-12-19 01:36:09 -0300

    fix arrow collision

--- Assets/Enviroment/Arrow_Object.gd
+++ Assets/Enviroment/Arrow_Object.gd
@@ -12,7 +12,7 @@ func _ready():
 func _physics_process(delta):
 	move_and_slide(direction * speed) 
 
-func _on_Area2D_body_entered(body):
+func _on_Arrow_Area_body_entered(body):
 	if body.is_in_group(Global.GROUPS.STATIC):
 		queue_free()
 	if body.is_in_group(Global.GROUPS.BOX):
@@ -22,4 +22,6 @@ func _on_Area2D_body_entered(body):
 	if body.is_in_group(Global.GROUPS.ENEMY):
 		queue_free()
 	if body.is_in_group(Global.GROUPS.TILEMAP):
+		queue_free()
+	if body.is_in_group(Global.GROUPS.ARROW):
 		queue_free()
