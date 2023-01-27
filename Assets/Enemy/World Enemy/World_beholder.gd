bbac1d63bf2281b7fb4e2fa358bf3545824941de
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-01-26 21:20:32 -0300

    wip raycast

--- Assets/Enemy/World Enemy/World_beholder.gd
+++ Assets/Enemy/World Enemy/World_beholder.gd
@@ -2,12 +2,15 @@ extends "res://Assets/Enemy/World Enemy/World_Enemy.gd"
 
 onready var obj = get_tree().current_scene.get_node("Player")
 
+var ray = RayCast2D.new()
+
 var direction = Vector2(-20, 20)
 var velocity = Vector2(20,20)
 var dir_frame = 9
 var telepor_dir = Vector2.ZERO
 func _ready():
 	$Enemy_Animation.stop(true)
+	self.add_child(ray)
 	ID = name
 	battle_unit_xp = 20
 	const_speed = 1
@@ -20,6 +23,10 @@ func _ready():
 	direction.x = rand_range(-20, 20)
 	direction.y = rand_range(-20, 20)
 
+func _process(delta):
+	if obj != null:
+		ray_cast()
+
 func _physics_process(delta):
 	#TODO create hits mechanics
 	$Sprite.frame = dir_frame
@@ -30,61 +37,25 @@ func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, speed * delta)
 	knockback = move_and_slide(knockback) 
 
+func init_ray_cast():
+	 ray.connect("body_entered", self, "_on_ray_collision")
+
+func ray_cast():
+	ray.cast_to = obj.get_global_position()
 
 func _on_Timer_timeout():
 	hit = false
 	timer.stop()
 
 func change_sprite_dir():
-	
-
-#
-#func _on_Area_Left_body_entered(body):
-#	if body.is_in_group(Global.GROUPS.PLAYER):
-#		$Enemy_Animation.play("Beholder_teleport_start")
-#		telepor_dir = obj.global_position
-#		dir_frame = 10
-#		speed = 0
-#		yield($Enemy_Animation, "animation_finished")
-#		action()
-#
-#func _on_Area_Right_body_entered(body):
-#	if body.is_in_group(Global.GROUPS.PLAYER):
-#		$Enemy_Animation.play("Beholder_teleport_start")
-#		telepor_dir = obj.global_position
-#		dir_frame = 12
-#		speed = 0
-#		yield($Enemy_Animation, "animation_finished")
-#		action()
-#
-#func _on_Area_Down_body_entered(body):
-#	if body.is_in_group(Global.GROUPS.PLAYER):
-#		$Enemy_Animation.play("Beholder_teleport_start")
-#		telepor_dir = obj.global_position
-#		dir_frame = 9
-#		speed = 0
-#		yield($Enemy_Animation, "animation_finished")
-#		action()
-#
-#func _on_Area_Up_body_entered(body):
-#	if body.is_in_group(Global.GROUPS.PLAYER):
-#		$Enemy_Animation.play("Beholder_teleport_start")
-#		telepor_dir = obj.global_position
-#		dir_frame = 11
-#		speed = 0
-#		yield($Enemy_Animation, "animation_finished")
-#		action()
-#
-#func _on_Area_Left_body_exited(body):
-#	if body.is_in_group(Global.GROUPS.PLAYER):
-#		dir_frame = 9
-#		speed = const_speed
-
+	pass
 
 func action():
 	pass
 
-
 func _on_Shoot_Timer_timeout():
 	action()
-	
+
+func _on_ray_collision(body):
+	# Handle the collision here
+	print("Collision with body:", body)
