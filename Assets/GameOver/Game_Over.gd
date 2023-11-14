4871a8e17dc19125fd2f9beaacb3a1b445cb5259
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-11-14 15:58:50 -0300

    fix ui game over

--- Assets/GameOver/Game_Over.gd
+++ Assets/GameOver/Game_Over.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 func _ready():
+	Ui.visible = false
 	$AnimationPlayer.play("game_over")
 
 
@@ -9,4 +10,5 @@ func _on_Button_pressed():
 	Global.player_last_position = Vector2.ZERO
 	Global.player_last_scene = ""
 	PlayerControll.restart()
+	Ui.visible = true
 	get_tree().change_scene("res://Assets/World/World_0.tscn")
