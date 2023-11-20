99e816cbc600bf7bda71280b379638718acd1e05
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-11-20 18:03:45 -0300

    wip

--- Assets/Enemy/Boss/World_Boss_Giant_Bat.gd
+++ Assets/Enemy/Boss/World_Boss_Giant_Bat.gd
@@ -1,7 +1,12 @@
 extends Node2D
-
 onready var player = get_tree().current_scene.get_node("Player")
 onready var positions = $Positions.get_children()
+
+var smoke = preload("res://Assets/Animations/smoke.tscn")
+var damageText = preload("res://Assets/UI/FloatText.tscn")
+
+var ID = name
+
 var direction = Vector2.ZERO
 var speed = 38
 var attack = false
@@ -9,8 +14,13 @@ var player_target = null
 var idle = false
 var target = null
 var rng = RandomNumberGenerator.new()
+var battle_unit_xp = 100
+var battle_unit_max_hp = 50
+var battle_unit_damage = 10
+var battle_unit_hp = battle_unit_max_hp
 
 func _ready():
+	add_to_group(Global.GROUPS.ENEMY)
 	find_new_position()
 
 func _process(_delta):
@@ -35,7 +45,7 @@ func move_to_another_point(_delta):
 
 func find_new_position():
 	rng.randomize()
-	var value = rng.randi_range(0, 2)
+	var value = rng.randi_range(0, positions.size()-1)
 	target = positions[value].position
 
 func _on_Attack_Timer_timeout():
@@ -55,3 +65,36 @@ func set_attack_values():
 	player_target = player.position
 	direction = (player_target - position).normalized()
 	attack = true
+
+func Destroy():
+	var temp_smoke = smoke.instance()
+	add_child(temp_smoke)
+	SoundController.play_effect(SoundController.EFFECTS.enemy_die)
+	yield(temp_smoke.get_node("AnimationPlayer"),"animation_finished")
+	Global.dead_enemies.push_front(ID)
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group(Global.GROUPS.ARROW):
+		damage(PlayerControll.atk+1)
+		body.queue_free()
+	if body.is_in_group(Global.GROUPS.BOMB):
+		damage(PlayerControll.atk+5)
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group(Global.GROUPS.SWORD):
+		damage(PlayerControll.atk)
+
+
+func damage(damageValue):
+		SoundController.play_effect(SoundController.EFFECTS.enemy_hit)
+		var text = damageText.instance()
+		text.set_text(str(damageValue))
+		add_child(text)
+		battle_unit_hp -= damageValue
+		$Animation_Player.play("damage_anim")
+		yield($Animation_Player, "animation_finished")
+		$Animation_Player.play("Boss_Giant_Bat_Normal")
+		if battle_unit_hp <= 0:
+			Destroy()
--- Assets/World/sub world/Imortal_Entities.gd
+++ Assets/World/sub world/Imortal_Entities.gd
@@ -1,31 +1,38 @@
-extends Node2D
+extends Area2D
 
 var mushroom = preload("res://Assets/Enemy/World Enemy/World_Mushroom.tscn")
 var rng = RandomNumberGenerator.new()
 var count = 1
+var collision = false
 
 func _ready():
 	rng.randomize()
 	count = rng.randi_range(1, 4)
-	print(count)
 	for i in count:
 		random_spawn()
 
+
 func random_spawn():
-	var random_position = Vector2(randi() % 225, randi() % 142)
-	var collision = is_position_ocupied(random_position)
+	var random_position = Vector2(randi() % 100, randi() % 70)
+	is_position_ocupied(random_position)
+	get_overlap_body()
+	print(random_position)
+	print(collision)
 	if not collision:
 		var monster = mushroom.instance()
 		monster.position = random_position
 		add_child(monster)
 	else:
-		$Check_If_Place_Free.queue_free()
 		random_spawn()
 	
 
+func get_overlap_body():
+	for node in get_overlapping_areas():
+		print(node)
+		if node:
+				print(node)
+				collision = true
+	collision = false
 
-func is_position_ocupied(position):
-	var area = $Check_If_Place_Free 
-	area.position = position
-	var collision = area.get_overlapping_bodies()
-	return collision
+func is_position_ocupied(random_position): 
+	position = random_position
