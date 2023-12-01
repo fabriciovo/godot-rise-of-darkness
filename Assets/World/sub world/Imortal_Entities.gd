69bfdbaf2207cb7e27d139e0173f7419e6d3f957
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-12-01 17:50:28 -0300

    wip

--- Assets/World/sub world/Imortal_Entities.gd
+++ Assets/World/sub world/Imortal_Entities.gd
@@ -1,22 +1,23 @@
-extends Area2D
+extends Node2D
 
+onready var positions = $Positions.get_children()
 var mushroom = preload("res://Assets/Enemy/World Enemy/World_Mushroom.tscn")
-var positions = $Positions.get_children()
 var rng = RandomNumberGenerator.new()
 var spawns_count = 0
-var position_number = 0
+var position_index = 0
 var collision = false
 
 func _ready():
 	rng.randomize()
 	spawns_count = rng.randi_range(1, 4)
-	position_number = rng.randi_range(0, positions.size())
 	for i in spawns_count:
 		random_spawn()
 
 
 func random_spawn():
-	var random_position = Vector2(randi() % 120, randi() % 50)
+	rng.randomize()
+	position_index = rng.randi_range(0, positions.size()-1)
+	var random_position = positions[position_index].position
 	var monster = mushroom.instance()
 	monster.position = random_position
 	add_child(monster)
