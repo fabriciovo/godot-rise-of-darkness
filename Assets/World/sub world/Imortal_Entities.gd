3210f0433b43c7772bede8563b92e47f4d99f2fe
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-12-01 16:47:55 -0300

    fix

--- Assets/World/sub world/Imortal_Entities.gd
+++ Assets/World/sub world/Imortal_Entities.gd
@@ -1,38 +1,22 @@
 extends Area2D
 
 var mushroom = preload("res://Assets/Enemy/World Enemy/World_Mushroom.tscn")
+var positions = $Positions.get_children()
 var rng = RandomNumberGenerator.new()
-var count = 1
+var spawns_count = 0
+var position_number = 0
 var collision = false
 
 func _ready():
 	rng.randomize()
-	count = rng.randi_range(1, 4)
-	for i in count:
+	spawns_count = rng.randi_range(1, 4)
+	position_number = rng.randi_range(0, positions.size())
+	for i in spawns_count:
 		random_spawn()
 
 
 func random_spawn():
-	var random_position = Vector2(randi() % 100, randi() % 70)
-	is_position_ocupied(random_position)
-	get_overlap_body()
-	print(random_position)
-	print(collision)
-	if not collision:
-		var monster = mushroom.instance()
-		monster.position = random_position
-		add_child(monster)
-	else:
-		random_spawn()
-	
-
-func get_overlap_body():
-	for node in get_overlapping_areas():
-		print(node)
-		if node:
-				print(node)
-				collision = true
-	collision = false
-
-func is_position_ocupied(random_position): 
-	position = random_position
+	var random_position = Vector2(randi() % 120, randi() % 50)
+	var monster = mushroom.instance()
+	monster.position = random_position
+	add_child(monster)
