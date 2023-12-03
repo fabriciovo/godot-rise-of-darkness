23ad08b57d2a5d17172abf1f065e22459bb67a3c
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-12-03 19:14:26 -0300

    wip transition

--- Shaders/Transition.gd
+++ Shaders/Transition.gd
@@ -1,17 +1,30 @@
-extends ColorRect
+extends CanvasLayer
+
+onready var transition = $Fill
+onready var animation = $Fill/Transition_Animator
+
+export (int, "Pixels", "Spot Player", "Spot Center","Slah V", "Slash H") var transition_type
+export (int, "Fade In", "Fade Out", "None") var execute
+var duration: float  = 0.5
+
 
 
 func _ready():
-	if Global.player_last_scene.find("Dungeon_") > 0:
+	if execute == 0:
+		transition.material.set_shader_param("type", transition_type)
+		fade_in()
+	if execute == 1:
+		transition.material.set_shader_param("type", transition_type)
 		fade_out()
 
+
 func fade_out():
-	$Transition_Animator.play("fade_out_anim")
-	yield ($Transition_Animator, "animation_finished")
+	animation.play("fade_out_anim")
+	yield (animation, "animation_finished")
 	
 func fade_in():
-	$Transition_Animator.play("fade_in_anim")
-	yield ($Transition_Animator, "animation_finished")
+	animation.play("fade_in_anim")
+	yield (animation, "animation_finished")
 	var scene_instance =  get_tree().change_scene("res://Assets/Battle/Battle.tscn")
 	if scene_instance != OK:
 		push_error("Scene not load")
