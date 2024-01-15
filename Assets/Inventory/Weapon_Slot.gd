05e55e14d29d796f8f96b9943208affd2150a852
Fabricio Varisco <fabricio.varisco@outlook.com>
2024-01-15 00:29:56 -0300

    WIP

--- Assets/Inventory/Weapon_Slot.gd
+++ Assets/Inventory/Weapon_Slot.gd
@@ -1,6 +1,23 @@
 class_name Weapon_Slot extends Button
 
-var weapon = 0
-func _ready():
-	pass 
+var sword_icon = preload("res://Sprites/Button/sword_btn_icon.png")
+var heal_icon = preload("res://Sprites/Button/healstaff_btn_icon.png")
+var bow_icon = preload("res://Sprites/Button/bow_btn_icon.png")
+var bomb_icon = preload("res://Sprites/Button/bomb_btn_icon.png")
+var shield_icon = preload("res://Sprites/Button/shield_btn_icon.png")
+var weapon_type = 0
 
+func set_weapon_type(_weapon_type):
+	if weapon_type == -1: return
+	weapon_type = _weapon_type
+	match weapon_type:
+		Global.WEAPONS.SWORD:
+			icon = sword_icon
+		Global.WEAPONS.HEAL:
+			icon = heal_icon
+		Global.WEAPONS.BOW:
+			icon = bow_icon
+		Global.WEAPONS.BOMB:
+			icon = bomb_icon
+		Global.WEAPONS.SHIELD:
+			icon = shield_icon
--- Assets/UI/Weapons.gd
+++ Assets/UI/Weapons.gd
@@ -4,8 +4,8 @@ onready var weapons_list = $Inventory_List/Weapons_Center_Container/Weapons_List
 
 func add_weapons():
 	if PlayerControll.items.size() > 0:
-		for weapon in weapons_list.size():
-				weapon = PlayerControll.items[weapon]
+		for i in range(min(PlayerControll.items.size(), weapons_list.size())):
+			weapons_list[i].set_weapon_type(PlayerControll.items[i])
 
 func _process(_delta):
 	add_weapons()
