553d7c0eca125f3bfe4db0fbb6ccfd376f32e2b9
Fabricio Varisco <fabricio.varisco@outlook.com>
2024-05-01 23:11:45 -0300

    wip start scene for select language

---
+++ Assets/Language Scene/Language_Scene.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_PT_focus_entered():
+	pass # Replace with function body.
+
+
+func _on_EN_focus_entered():
+	pass # Replace with function body.
