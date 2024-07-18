6684fd1c7a105106f9db0d9a85fa64557432a81a
Fabricio Oliveira <13788356+fabriciovo@users.noreply.github.com>
2024-07-17 21:32:26 -0300

    fix block doors

--- Assets/Actions/Block_Door_Can_Go_Back.gd
+++ Assets/Actions/Block_Door_Can_Go_Back.gd
@@ -1,15 +1,9 @@
 extends StaticBody2D
 var can_pass = false
+
 func _ready():
-	if PlayerControll.inventory.size() > 0:
-		queue_free()
 	add_to_group(Global.GROUPS.DOOR_WITH_INTERACTION)
 
-func _process(_delta):
-	if PlayerControll.inventory.size() == 0:
-		can_pass = false
-	else:
-		can_pass = true
 
 func trigger_dialog_box():
 	Global.stop = true
