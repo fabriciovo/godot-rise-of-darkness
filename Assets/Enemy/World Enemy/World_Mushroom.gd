447bf9449eefd632186257444c11928c01adcf07
Fabricio Oliveira <13788356+fabriciovo@users.noreply.github.com>
2024-08-08 10:55:40 -0300

    wip

--- Assets/Enemy/World Enemy/World_Mushroom.gd
+++ Assets/Enemy/World Enemy/World_Mushroom.gd
@@ -38,16 +38,9 @@ func _process(_delta):
 		if chase_player:
 			has_to_stop = true
 		return
-	if dead:
-		speed = 0
-		if hits > 1:
-			PlayerControll.set_xp(battle_unit_xp)
-		yield(enemy_player, "animation_finished")
-		var temp_smoke = smoke.instance()
-		temp_smoke.global_position = global_position
-		get_tree().get_current_scene().add_child(temp_smoke)
-		SoundController.play_effect(SoundController.EFFECTS.enemy_die)
-		queue_free()
+	if dead: return
+	if hit:
+		explosion()
 	if has_to_stop and chase_player:
 		has_to_stop = false
 		mushroom_player.play("mushroom_start_explosion",-1,1,current_anim_pos)
@@ -55,27 +48,22 @@ func _process(_delta):
 func _physics_process(_delta):
 	if Global.stop: return
 	if player == null or navigation_path == null: return
-	if dead: return
-	if battle_unit_hp <= 0:
-		dead = true
-	if hit:
-		explosion()
-	if not dead and not hit:
-		if player and navigation_path and chase_player:
-			generate_path()
-			navigate()
-		else: 
-			var collision = move_and_collide(direction * speed * _delta)
-			if collision:
-				direction = direction.bounce(collision.normal)
-				velocity = direction
-		velocity = move_and_slide(velocity)
-		explosion_after_anim()
+	if dead or hit: return
+	if player and navigation_path and chase_player:
+		generate_path()
+		navigate()
+	else: 
+		var collision = move_and_collide(direction * speed * _delta)
+		if collision:
+			direction = direction.bounce(collision.normal)
+			velocity = direction
+	velocity = move_and_slide(velocity)
+	explosion_after_anim()
 
 func explosion_after_anim():
 	if mushroom_player.get_current_animation() == "mushroom_start_explosion":
 		yield(mushroom_player, "animation_finished")
-		explosion()
+		dead = true
 
 func generate_path():
 	paths = navigation_path.get_simple_path(global_position, player.global_position,false)
@@ -101,8 +89,15 @@ func _on_Chase_Area_body_exited(body):
 
 func explosion():
 	dead = true
+	speed = 0
 	if hits > 1:
+		yield(enemy_player, "animation_finished")
 		PlayerControll.set_xp(battle_unit_xp)
+	var temp_smoke = smoke.instance()
+	temp_smoke.global_position = global_position
+	get_tree().get_current_scene().add_child(temp_smoke)
+	SoundController.play_effect(SoundController.EFFECTS.enemy_die)
+	queue_free()
 
 func _on_Explosion_Area_body_entered(body):
 	if body.is_in_group(Global.GROUPS.PLAYER):
