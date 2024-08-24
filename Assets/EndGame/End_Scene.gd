ca428f632ef93a231dbdf4ef124cfca502c30e8c
Fabricio Varisco <fabricio.varisco@outlook.com>
2024-08-23 22:18:43 -0300

    wip

--- Assets/EndGame/End_Scene.gd
+++ Assets/EndGame/End_Scene.gd
@@ -3,14 +3,14 @@ class_name End_Scene extends Node2D
 
 onready var transition = $Transition
 onready var thunder = $Thunder
-
+onready var monster = $Monsters
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-
 func _on_Timer_timeout():
 	thunder.play_flash()
-	yield(thunder.animation, "finished_animation")
+	yield(get_tree().create_timer(1.1), "timeout")
+	monster.queue_free()
--- Assets/EndGame/Npc_Ending.gd
+++ Assets/EndGame/Npc_Ending.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export(String, "alt_sides_1","alt_sides_2", "alt_wood_sides" ) var anim_name = "alt_sides_1"
+export(String, "alt_sides_1","alt_sides_2", "alt_wood_side" ) var anim_name
 
 onready var anim = $Animation_Player
 var smoke = preload("res://Assets/Animations/smoke.tscn")
