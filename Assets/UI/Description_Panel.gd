cc60c39b3c5d446d11b42b08db8ccb2bb8c537b0
Fabricio Oliveira <13788356+fabriciovo@users.noreply.github.com>
2024-09-02 07:30:35 -0300

    Update Description_Panel.gd

--- Assets/UI/Description_Panel.gd
+++ Assets/UI/Description_Panel.gd
@@ -5,16 +5,16 @@ onready var label = $Description_Label
 
 func _on_upgrade_hp_focus_entered():
 	title.text = "MAX_HP_INFO_TITLE"
-	label.text = "MAX_HP_INFO_DESCRIPTION"
+	label.text = "MAX_HP_DESCRIPTION"
 
 func _on_upgrade_Magic_focus_entered():
 	title.text = "MAX_MP_INFO_TITLE"
-	label.text = "MAX_MP_INFO_DESCRIPTION"
+	label.text = "MAX_MP_DESCRIPTION"
 
 func _on_upgrade_action_points_focus_entered():
 	title.text = "AP_INFO_TITLE"
-	label.text = "AP_INFO_DESCRIPTION"
+	label.text = "AP_DESCRIPTION"
 
 func _on_upgrade_atk_focus_entered():
 	title.text = "ATK_INFO_TITLE"
-	label.text = "ATK_INFO_DESCRIPTION"
+	label.text = "ATK_DESCRIPTION"
