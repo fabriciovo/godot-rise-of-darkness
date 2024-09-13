d3ff93affa1e4fa6a3a32e8998b6b78fa496bc67
Fabricio Varisco <fabricio.varisco@outlook.com>
2024-09-12 23:39:42 -0300

    fix ending stop music when go back to title

--- Assets/EndGame/Scrolling_End_Text.gd
+++ Assets/EndGame/Scrolling_End_Text.gd
@@ -6,6 +6,7 @@ func _process(_delta):
 	if(position.y <= -910):
 		parent.speed = 0
 		if (Input.is_action_just_released("start")):
+			SoundController.stop_music()
 			var _scene_instance = get_tree().change_scene("res://Assets/TitleScene/Title_Scene.tscn")
 	else:
 		position.y -= parent.speed * _delta
