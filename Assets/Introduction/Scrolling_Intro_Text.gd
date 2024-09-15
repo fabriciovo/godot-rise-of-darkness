68c494227327ebc701052142f6163353dae2bbae
Fabricio Varisco <fabricio.varisco@outlook.com>
2024-09-15 11:05:26 -0300

    fix intro

--- Assets/Introduction/Scrolling_Intro_Text.gd
+++ Assets/Introduction/Scrolling_Intro_Text.gd
@@ -4,7 +4,7 @@ onready var parent = get_parent()
 
 func _process(_delta):
 	position.y -= parent.speed * _delta
-	if(position.y <= -1200):
+	if(position.y <= -600):
 		var scene_instance = get_tree().change_scene("res://Assets/World/World_0.tscn")
 		if scene_instance == OK:
 			Ui.game_start()
